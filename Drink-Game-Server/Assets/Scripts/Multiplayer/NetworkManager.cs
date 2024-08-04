f459d4d35a75253c4518ccce8128515db09579fa
K4rm45 <galsilard55@gmail.com>
2024-08-04 02:20:34 +0200

    Server and Client CONNECT

---
+++ Drink-Game-Server/Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,28 @@
+using InexperiencedDeveloper.Core;
+using Riptide;
+using Riptide.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Singleton<NetworkManager> 
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, true);
+    }
+    public Server Server;
+    [SerializeField] private ushort m_PORT = 7777;
+    [SerializeField] private ushort m_MaxPlayers = 5;
+
+    private void Start()
+    {
+        Server = new Server();
+        Server.Start(m_PORT, m_MaxPlayers);
+    }
+    private void FixedUpdate()
+    {
+        Server.Update();
+    }
+}
